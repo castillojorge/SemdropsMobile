ace675fb00923ee1bcd480e327a80571945a62d4
castillojorge <castillo.jorge.08@gmail.com>
2011-08-18 14:51:50 -0300

    semdropsUpdate

--- SymfonySemdropsMobile/app/cache/dev/twig/e2/59/a7ec437909e526c7ce5631b89462.php
+++ SymfonySemdropsMobile/app/cache/dev/twig/e2/59/a7ec437909e526c7ce5631b89462.php
@@ -3,8 +3,6 @@
 /* SemdropsSemdropsMobileBundle:Semdrops:showtags.html.twig */
 class __TwigTemplate_e259a7ec437909e526c7ce5631b89462 extends Twig_Template
 {
-    protected $parent;
-
     public function __construct(Twig_Environment $env)
     {
         parent::__construct($env);
@@ -16,47 +14,46 @@ class __TwigTemplate_e259a7ec437909e526c7ce5631b89462 extends Twig_Template
         );
     }
 
-    public function getParent(array $context)
-    {
-        if (null === $this->parent) {
-            $this->parent = $this->env->loadTemplate("::base.html.twig");
-        }
-
-        return $this->parent;
-    }
-
     protected function doDisplay(array $context, array $blocks = array())
     {
         $context = array_merge($this->env->getGlobals(), $context);
 
-        $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
+        // line 1
+        $this->displayBlock('title', $context, $blocks);
+        // line 2
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 3
+        $this->displayBlock('body', $context, $blocks);
+        // line 14
+        echo "
+";
     }
 
-    // line 3
+    // line 1
     public function block_title($context, array $blocks = array())
     {
         echo " Semdrops Mobile - Results ";
     }
 
-    // line 4
+    // line 2
     public function block_stylesheets($context, array $blocks = array())
     {
     }
 
-    // line 5
+    // line 3
     public function block_body($context, array $blocks = array())
     {
-        // line 6
+        // line 4
         echo "\tCategories of the wiki <b>";
         echo twig_escape_filter($this->env, $this->getAttribute($this->getContext($context, 'filledLink'), "getUri", array(), "method", false), "html");
         echo "</b>:<br>
 \t";
-        // line 7
+        // line 5
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getContext($context, 'filledLink'), "getCategories", array(), "method", false));
         $context['_iterated'] = false;
         foreach ($context['_seq'] as $context['_key'] => $context['result']) {
-            // line 8
+            // line 6
             echo "\t\t";
             echo twig_escape_filter($this->env, $this->getContext($context, 'result'), "html");
             echo "<br>
@@ -64,21 +61,21 @@ class __TwigTemplate_e259a7ec437909e526c7ce5631b89462 extends Twig_Template
             $context['_iterated'] = true;
         }
         if (!$context['_iterated']) {
-            // line 9
+            // line 7
             echo " ";
-            // line 10
+            // line 8
             echo "\t\tNo categories.<br>
 \t";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['result'], $context['_parent'], $context['loop']);
         $context = array_merge($_parent, array_intersect_key($context, $_parent));
-        // line 12
+        // line 10
         echo "\t<input type= \"button\" onclick= \"location.href='";
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("addCategory"), "html");
         echo "'\" value=\"Add a Category\" name=\"addCategoryButton\" /><br>
 \t<input type= \"button\" onclick= \"location.href='";
-        // line 13
+        // line 11
         echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getPath("homepage"), "html");
         echo "'\" value=\"Go Back to Main\" name=\"gobackButton\" />
 
@@ -92,6 +89,6 @@ class __TwigTemplate_e259a7ec437909e526c7ce5631b89462 extends Twig_Template
 
     public function isTraitable()
     {
-        return false;
+        return true;
     }
 }
