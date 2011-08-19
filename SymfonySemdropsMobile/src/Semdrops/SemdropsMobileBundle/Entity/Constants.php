2a476533b8ad992187635ac0a7ae3c89a848471e
yukakami <yukari.kaminose@gmail.com>
2011-08-19 14:03:02 -0300

    Un comentario para aclarar

--- SymfonySemdropsMobile/src/Semdrops/SemdropsMobileBundle/Entity/Constants.php
+++ SymfonySemdropsMobile/src/Semdrops/SemdropsMobileBundle/Entity/Constants.php
@@ -4,6 +4,6 @@
 	class Constants {
 		public $BASECONSULTA= "http://requiem:8080/sesame/repositories/lalala/query?";
 		public $BASEINSERCION= "http://requiem:8080/openrdf-sesame/repositories/lalala/statements";
-		public $TREEDEPTH= 3; //profundidad del arbol de categorias
+		public $TREEDEPTH= 2; //profundidad del arbol de categorias, cantidad de padres
 	}
 ?>
