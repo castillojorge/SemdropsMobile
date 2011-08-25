3d07c227c80ca7b78934acf4b1e0619dbdf665a5
castillojorge <castillo.jorge.08@gmail.com>
2011-08-25 15:50:41 -0300

    Test

---
+++ SymfonySemdropsMobile/src/Semdrops/SemdropsMobileBundle/Tests/writeInSesameDataBaseTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Semdrops\SemdropsMobileBundle\Tests;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Semdrops\SemdropsMobileBundle\Entity\functions;
+
+Class writeInSesameDataBaseTest extends TestCase
+{
+   protected $semdrops;
+   
+  public function setUp()
+   {
+    $this -> semdrops = new functions();
+    }
+   public function testWriteInSesameDataBase()
+   {
+	$url='http://requiem:8080/openrdf-sesame/repositories/lalala/statements';
+	$datos='<http://es.wikipedia.org/wiki/Argentina> <rdf:Type> <http://semdrops.lifia.edu.ar/ns/category#TestPais>.';   
+	$this -> assertTrue($this->semdrops->writeInSesameDataBase($datos));
+	}
+    public function testSesameDataBaseFailure()
+ {
+    $url='http://failure:8080/openrdf-sesame/repositories/lalala/statements';
+	$datos='<http://es.wikipedia.org/wiki/Argentina> <rdf:Type> <http://semdrops.lifia.edu.ar/ns/category#TestPais>.';   
+    $this-> assertFalse($this->semdrops->writeInSesameDataBase($datos));
+  }
+ }
+?>
