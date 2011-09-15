fc2cc0b2e4930b3ef4ebeff2c3b31a07d1c2575d
castillojorge <castillo.jorge.08@gmail.com>
2011-09-15 15:31:34 -0300

    SemdposViews

---
+++ SymfonySemdropsMobile/src/Semdrops/SemdropsMobileBundle/Entity/Constants.php
@@ -0,0 +1,9 @@
+<?php
+	namespace Semdrops\SemdropsMobileBundle\Entity;
+	
+	class Constants {
+		public $BASECONSULTA= "http://requiem.local:8080/openrdf-workbench/repositories/lalala/query?";
+		public $BASEINSERCION= "http://requiem.local:8080/openrdf-sesame/repositories/lalala/statements";
+		public $TREEDEPTH= 2; //profundidad del arbol de categorias, cantidad de padres
+	}
+?>
---
+++ SymfonySemdropsMobile/src/Semdrops/SemdropsMobileBundle/Entity/Sesame.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Semdrops\SemdropsMobileBundle\Entity;
+use Semdrops\SemdropsMobileBundle\Entity\Constants;
+class Sesame {
+	
+	private $urls = new Constants();
+	
+	    	
+	public function writeACategory($aCategory) {
+    $datos= '<'.$aCategory->getUri().'> <rdf:Type> <http://semdrops.lifia.edu.ar/ns/category#'.$aCategory->getCategory().'>.';//Se arman los datos para guardarlos en la base de datos
+    return $this-> writeInSesameDataBase($datos);	
+    }
+    
+	public function writeAProperty($aProperty) {
+	$datos= '<'.$aProperty->getUri().'> <http://semdrops.lifia.edu.ar/ns/property#'.$aProperty->getPropertyTag().'> <'.$aProperty->getDestino().'>.';
+    return $this-> writeInSesameDataBase($datos);
+	} 
+	
+	function writeInSesameDataBase($datos) {
+       $request_body =$datos;
+       $ch = curl_init($urls->BASEINSERCION);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
+       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);//True
+       curl_setopt($ch, CURLOPT_HEADER, 0);//false
+       curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/plain', 'charset=UTF-8'));
+       curl_setopt($ch, CURLOPT_POST, TRUE); 
+       //curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//true
+       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+       $response = curl_exec($ch);
+       curl_close($ch);
+       return $response;
+	}
+}
